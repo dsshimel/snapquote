d6c2e4537c35cd7b220de37e7397a7a8b39b0975
David <dsshimel@gmail.com>
2013-06-15 19:41:28 -0400

    extending admin page

---
+++ snapquote/sq_web/admin.py
@@ -0,0 +1,12 @@
+from sq_web.models import *
+from django.contrib import admin
+
+admin.site.register(Patron)
+admin.site.register(Address)
+admin.site.register(PhoneNumber)
+admin.site.register(Shop)
+admin.site.register(Car)
+admin.site.register(Incident)
+admin.site.register(Photo)
+admin.site.register(ShopIncident)
+admin.site.register(Quote)
