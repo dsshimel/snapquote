034fb5832993d50d5ef6f94d04edaf8458c7b6d1
David <dsshimel@gmail.com>
2013-06-15 15:04:02 -0400

    initial models set up, MEDIA_ROOT for incident photos

--- snapquote/snapquote/settings.py
+++ snapquote/snapquote/settings.py
@@ -1,4 +1,8 @@
 # Django settings for snapquote project.
+import os, django
+
+DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -50,7 +54,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -111,6 +115,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(SITE_ROOT, '../sq_web/templates')
 )
 
 INSTALLED_APPS = (
@@ -124,6 +129,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'sq_web',
 )
 
 # A sample logging configuration. The only tangible logging
--- snapquote/sq_web/models.py
+++ snapquote/sq_web/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Patron(models.Model):
+	user = models.OneToOneField(User)
+
+	def __unicode__(self):
+		return self.first_name + ' ' + self.last_name
+
+class Address(models.Model):
+	line_one = models.CharField(max_length=100)
+	line_two = models.CharField(max_length=100)
+	city = models.CharField(max_length=100)
+	state = models.CharField(max_length=100)
+	zipcode = models.IntegerField()
+	zipextension = models.IntegerField(null=True)
+
+class PhoneNumber(models.Model):
+	areacode = models.IntegerField()
+	phone = models.IntegerField()
+
+	def __unicode__(self):
+		return '(' + str(self.areacode) + ')-' + str(self.phone)[:3] + '-' + str(self.phone)[3:]
+
+class Shop(models.Model):
+	user = models.OneToOneField(User)
+	name = models.CharField(max_length=100)
+	address = models.OneToOneField(Address)
+	phone = models.OneToOneField(PhoneNumber)
+
+	def __unicode__(self):
+		return self.name
+
+class Car(models.Model):
+	owner = models.ForeignKey(Patron)
+	make = models.CharField(max_length=50)
+	model = models.CharField(max_length=50)
+	color = models.CharField(max_length=50)
+	year = models.IntegerField()
+
+class Incident(models.Model):
+	patron = models.ForeignKey(Patron)
+	occurred = models.DateTimeField(auto_now_add=True)
+	main_photo = models.OneToOneField('Photo', related_name='incident_main_photo')
+	car = models.OneToOneField(Car)
+
+class Photo(models.Model):
+	image = models.ImageField(upload_to='incidents/%Y/%m/%d')
+	incident = models.ForeignKey(Incident)
+
+class ShopIncident(models.Model):
+	shop = models.OneToOneField(Shop)
+	incident = models.ForeignKey(Incident)
+	is_favorite = models.BooleanField(default=False)
+
+class Quote(models.Model):
+	amount = models.DecimalField(max_digits=8, decimal_places=2)
+	incident = models.ForeignKey(Incident)
+	shop_incident = models.OneToOneField(ShopIncident)
+	comments = models.TextField()
